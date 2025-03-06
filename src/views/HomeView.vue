b98bdc3db41d9bdc2e4082d23a15f8457bd21911
adrianrakiec <rakiecadrian17@gmail.com>
2025-03-06 09:42:39 +0100

    install ant-design-vue and create static home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,43 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>
-    <h1>Weather App</h1>
-  </main>
+  <a-layout-header class="header">
+    <a-typography-title :level="2" class="title"> 🌤️Weather App </a-typography-title>
+  </a-layout-header>
+
+  <a-layout-content>
+    <div class="content">
+      <a-auto-complete v-model:value="search" placeholder="Search city..." class="search-input" />
+      <a-button><SearchOutlined /></a-button>
+      <a-button type="primary" @click="useCurrentLocation">
+        <EnvironmentOutlined />
+      </a-button>
+    </div>
+  </a-layout-content>
 </template>
+
+<script setup lang="ts">
+import { EnvironmentOutlined } from '@ant-design/icons-vue'
+import { SearchOutlined } from '@ant-design/icons-vue'
+import { ref } from 'vue'
+
+const search = ref('')
+
+function useCurrentLocation() {
+  alert('Your location')
+}
+</script>
+
+<style scoped>
+.header {
+  text-align: center;
+}
+
+.title {
+  padding: 1em;
+}
+
+.content {
+  display: flex;
+  gap: 0.1em;
+  padding: 0 1em;
+}
+</style>
