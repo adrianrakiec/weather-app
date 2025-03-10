5280e7d44243443e027ea8ee881b25660bb8fd5b
adrianrakiec <rakiecadrian17@gmail.com>
2025-03-10 10:04:49 +0100

    create notification stotre

---
+++ src/stores/useNotificationStore.ts
@@ -0,0 +1,42 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useNotificationStore = defineStore('notification', () => {
+  const message = ref<string | null>(null)
+  const type = ref<'success' | 'error' | null>(null)
+
+  const clearMessage = () => {
+    message.value = null
+    type.value = null
+  }
+
+  const clearAfterTimeout = (timeout: number) => {
+    setTimeout(clearMessage, timeout)
+  }
+
+  const showSuccess = (msg: string, timeout = 2000) => {
+    message.value = msg
+    type.value = 'success'
+    clearAfterTimeout(timeout)
+  }
+
+  const showError = (msg: string, timeout = 2000) => {
+    message.value = msg
+    type.value = 'error'
+    clearAfterTimeout(timeout)
+  }
+
+  const handleError = (error: unknown, timeout = 2000) => {
+    let errorMessage = 'Unexpected error occurred'
+
+    if (error instanceof Error) {
+      errorMessage = error.message
+    } else if (typeof error === 'string') {
+      errorMessage = error
+    }
+
+    showError(errorMessage, timeout)
+  }
+
+  return { message, type, showSuccess, handleError }
+})
